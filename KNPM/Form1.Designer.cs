db54152edad4889eb5bfbaeef8bb3defb8739ff2
Linh Ken <silinh66@gmail.com>
2021-03-05 22:42:38 +0700

    Add project files.

---
+++ KNPM/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace KNPM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTK = new System.Windows.Forms.TextBox();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(85, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG NHẬP";
+            // 
+            // txtTK
+            // 
+            this.txtTK.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtTK.Location = new System.Drawing.Point(123, 82);
+            this.txtTK.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
+            this.txtTK.MaximumSize = new System.Drawing.Size(200, 100);
+            this.txtTK.MinimumSize = new System.Drawing.Size(100, 20);
+            this.txtTK.Multiline = true;
+            this.txtTK.Name = "txtTK";
+            this.txtTK.PlaceholderText = "Tên người dùng";
+            this.txtTK.Size = new System.Drawing.Size(198, 36);
+            this.txtTK.TabIndex = 1;
+            this.txtTK.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtMK
+            // 
+            this.txtMK.Location = new System.Drawing.Point(123, 149);
+            this.txtMK.Multiline = true;
+            this.txtMK.Name = "txtMK";
+            this.txtMK.PasswordChar = '*';
+            this.txtMK.PlaceholderText = "Mật khẩu";
+            this.txtMK.Size = new System.Drawing.Size(198, 37);
+            this.txtMK.TabIndex = 2;
+            this.txtMK.UseSystemPasswordChar = true;
+            this.txtMK.TextChanged += new System.EventHandler(this.txtMK_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SeaGreen;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(123, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 25);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ĐĂNG NHẬP";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(123, 240);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 27);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "THOÁT";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 328);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMK);
+            this.Controls.Add(this.txtTK);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ĐĂNG NHẬP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTK;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ KNPM/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace KNPM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-3L4FS0A\SQLEXPRESS;Initial Catalog=KNPM;Integrated Security=True");
+            try
+            {
+                con.Open();
+                string tk = txtTK.Text;
+                string mk = txtMK.Text;
+                string sql = "select * from TAIKHOAN where TAIKHOAN = '" + tk + "' and MATKHAU = '" + mk + "'";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                SqlDataReader dta = cmd.ExecuteReader();
+                if (dta.Read() == true)
+                {
+                    MessageBox.Show("Đăng NHẬP THÀNH CÔNG");
+                    this.Hide();
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                }
+                else
+                {
+                    MessageBox.Show("ĐĂNG NHẬP THẤT BẠI");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối");
+            }
+        }
+            private void butThoat_Click(object sender, EventArgs e)
+            {
+                Application.Exit();
+            }
+
+        private void txtMK_TextChanged(object sender, EventArgs e)
+        {
+            txtMK.PasswordChar = '*';
+        }
+    }
+    }
+
